996defdaabe41f5125b43f5d4ac91cbb84d92de1
Keshavcodes08 <ranakeshav2025@gmail.com>
2026-08-09 16:35:37 +0530

    Create 05-operators.c
    learned how to use arithmetic operators.

---
+++ 05-operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+        int n1,n2;
+        printf("Enter number 1:\t");
+        scanf("%d",&n1);
+        printf("Enter number 2:\t");
+        scanf("%d",&n2);
+        {
+                printf("%d + %d = %d\n", n1, n2, n1 + n2);
+                printf("%d - %d = %d\n", n1, n2, n1 - n2);
+printf("%d * %d = %d\n", n1, n2, n1 * n2);
+printf("%d / %d = %d\n", n1, n2, n1 / n2);
+printf("%d %% %d = %d\n", n1, n2, n1%n2);
+        }
+        return 0;
+}
