72bc388a4a541c4f819de586dffc30950bd54cfd
Keshavcodes08 <ranakeshav2025@gmail.com>
2026-08-09 16:39:45 +0530

    Create 09-for_loop.c
    learned how to use for loop.

---
+++ 09-for_loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+        int no;
+        no=10;
+        int i;
+        for(i=1;i<=no;i++)
+        {
+                printf("%d\n" ,i);
+        }
+        return 0;
+}
