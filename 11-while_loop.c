9b0cf3d87c7d07f175b4cecda5bf85c5d293350f
Keshavcodes08 <ranakeshav2025@gmail.com>
2026-08-09 16:48:39 +0530

    Create 11-while_loop.c
    randomly stumbled upon while loop.

---
+++ 11-while_loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+	int n;
+	printf("Enter a number:\t");
+	scanf("%d",&n);
+	int i;
+	i=n;
+	while(i<=10)
+	{
+			printf("%d\n",i);
+			i++;
+	}
+return 0;
+}
+		
