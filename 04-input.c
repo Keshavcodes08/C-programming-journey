9c110344d7faae715be3c907c4dedd9c00ba6e35
Keshavcodes08 <ranakeshav2025@gmail.com>
2026-08-09 16:34:22 +0530

    Create 04-input.c
    Learned how to get user input.

---
+++ 04-input.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+        int age;
+        int marks;
+        {
+        printf("Enter Your Age:\t");
+        scanf("%d",&age);
+        printf("Enter Your Marks:\t");
+        scanf("%d",&marks);
+}
+return 0;
+}
