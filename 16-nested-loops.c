594cd2568a09c9d45511e1dc2657f6d676b40480
Keshavcodes08 <ranakeshav2025@gmail.com>
2026-08-10 07:08:58 +0530

    Create 16-nested-loops.c
    learned to use 3 variable loop and create a new pattern (tree).

---
+++ 16-nested-loops.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<ctype.h>	
+int main()
+{
+	int i,j,k,n;
+	printf("Enter any number:\t\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(k=n;k>=i;k--)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+				printf("\n");
+		}
+	return 0;
+}
