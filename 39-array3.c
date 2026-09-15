d87919b0b346be40bab29f56bd16735fbcf2c68f
Keshavcodes08 <ranakeshav2025@gmail.com>
2026-09-15 13:43:50 +0530

    Create 39-array3.c
    created a 2d array with input and display.

---
+++ 39-array3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int arr[2][2];
+	int i,j;
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("Enter no. ");
+			scanf("%d",&arr[i][j]);
+		}
+		printf("\n");
+	}
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("%d\t",arr[i][j]);
+		}
+		printf("\n");
+	}
+		return 0;
+}
